8aed5d771032428c4c4a64a8480d091316fda8b3
banno <84123831+Arpit957@users.noreply.github.com>
2023-09-21 15:12:04 +0530

    Update 4Leetcode.cpp

--- 4Leetcode.cpp
+++ 4Leetcode.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
+    double mediann(vector<int>&a,vector<int>&b){
+        int m=a.size();
+        int n=b.size();
+        if(m>n)
+            return mediann(b,a);
+        int l=0,r=m;
+        while(l<=r){
+            int partx=l+(r-l)/2;
+            int party=(m+n+1)/2-partx;
+            int maxlx=(partx==0)?INT_MIN:a[partx-1];
+            int minrx=(partx==m)?INT_MAX:a[partx];
+            int maxly=(party==0)?INT_MIN:b[party-1];
+            int minry=(party==n)?INT_MAX:b[party];
+            if(maxlx<=minry&&maxly<=minrx){
+                if((m+n)%2==0)
+                    return (double)(max(maxlx,maxly)+min(minrx,minry))/2;
+                else
+                    return (double)(max(maxlx,maxly));
+            }else if(maxlx>minry)
+                r=partx-1;
+            else
+                l=partx+1;
+        }
+        return -1.0;
+    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-          priority_queue<int>pq;
-        for(auto &x:nums1){
-            pq.push(x);
-        }
-        
-        for(auto &x:nums2){
-            pq.push(x);
-        }
-        
-        int n=pq.size();
-        for(int i=0; i<(n/2)-1; ++i){
-            pq.pop();
-        }
-        
-         double res=0.0;
-        if(n&1){
-            pq.pop();
-            res+=pq.top();
-        }
-        else {
-            res+=pq.top();
-            pq.pop();
-            res+=pq.top();
-            res/=2;
-        }
-        
-        return res;
+        double ans;
+        ans=mediann(nums1,nums2);
+        return ans;   
     }
 };
